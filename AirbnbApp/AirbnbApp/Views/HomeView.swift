d2ba4dbe0cbdf7287f37408c4240a94364f957c0
Oriol Angelet <uriaeri@gmail.com>
2023-12-06 16:31:59 +0100

    Final commit of the course

--- AirbnbApp/AirbnbApp/Views/HomeView.swift
+++ AirbnbApp/AirbnbApp/Views/HomeView.swift
@@ -1,16 +1,35 @@
 //
-//  ContentView.swift
+//  AirbnbListingsView.swift
 //  AirbnbApp
-//  
+//
 //  Created by Uri on 5/12/23.
 //
 
 import SwiftUI
 
 struct HomeView: View {
-
+    
+    @StateObject var viewModel = AirbnbListingsViewModel()
+    
     var body: some View {
-        ListingsView()
+        NavigationStack {
+            VStack {
+                if viewModel.listings.isEmpty {
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle())
+                } else {
+                    List(viewModel.listings) { listing in
+                        NavigationLink(destination: ListingDetailView(model: listing), label: {
+                            ListingCardView(model: listing)
+                        })
+                    }
+                }
+            }
+            .navigationTitle("Airbnb")
+        }
+        .onAppear {
+            viewModel.fetchListings()
+        }
     }
 }
 
--- AirbnbApp/AirbnbApp/Views/ListingDetailView.swift
+++ AirbnbApp/AirbnbApp/Views/ListingDetailView.swift
@@ -11,16 +11,51 @@ struct ListingDetailView: View {
     let model: AirbnbListing
     
     var body: some View {
-        ScrollView(.vertical) {
-            AsyncImage(url: URL(string: model.xlPictureURL ?? ""))
-                .aspectRatio(contentMode: .fill)
-                .clipped()
-            
-            Text("Name: \(model.name ?? "")")
-            Text("Description: \(model.description ?? "")")
-            Text("Summary: \(model.summary ?? "")")
-            Text("House rules: \(model.houseRules ?? "")")
-            Text("Space: \(model.space ?? "")")
+        VStack(alignment: .leading) {
+            GeometryReader { proxy in
+                ScrollView(.vertical) {
+                    AsyncImage(url: URL(string: model.xlPictureURL ?? ""))
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: proxy.frame(in: .global).width, height: proxy.frame(in: .global).height)
+                        .clipped()
+                    
+                    Text(model.name ?? "")
+                        .bold()
+                        .padding()
+                    
+                    if let price = model.price {
+                        Text("Nightly Rate: \(price.formatted(.currency(code: "USD")))")
+                    }
+                    
+                    Text("Description: \(model.description ?? "")")
+                        .padding()
+                    Text("Summary: \(model.summary ?? "")")
+                        .padding()
+                    Text("House Rules: \(model.houseRules ?? "")")
+                        .padding()
+                    Text("Space: \(model.space ?? "")")
+                        .padding()
+                    
+                    Text("About Your Host")
+                        .font(.title2)
+                        .bold()
+                    
+                    HStack {
+                        AsyncImage(url: URL(string: model.hostThumbnailURL ?? ""))
+                            .frame(width: 75, height: 75)
+                            .aspectRatio(contentMode: .fill)
+                            .clipShape(Circle())
+                        
+                        Text(model.hostName ?? "")
+                            .bold()
+                    }
+                    .padding()
+                    
+                    Text("Hosting since: \(model.hostSince ?? "")")
+                }
+                .frame(maxWidth: proxy.frame(in: .local).width)
+            }
         }
+        .navigationTitle(model.name ?? "Listing")
     }
 }
